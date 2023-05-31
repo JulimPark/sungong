eb3e989d065a416c63ac65b10cd8716867cfdb96
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-06-01 05:16:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ def push_start_data2(stu_id):
     aaa = datetime.now()
     aaa = aaa.astimezone(timezone('Asia/Seoul'))
     timestamp1 = aaa.timestamp()
-    temp_dict = {'시작시간':aaa,'시작스탬프':timestamp1}
+    temp_dict = {'시작시간':aaa,'시작스탬프':timestamp1,'마침시간':'-','마침스탬프':'-'}
     
     df11 = pd.DataFrame(temp_dict, index=[0])
     df11['시작시간']= pd.to_datetime(df11['시작시간'])
     
     df11.to_csv('temp_csv.csv',index=False,mode='w')
     st1 = datetime.fromtimestamp(timestamp1)
-    st.write(f"시작시간: {aaa.year}-{aaa.month}-{aaa.day} {aaa.hour}:{aaa.minute}:{aaa.second}")
+    st.write(f"시작시간: {aaa}")
 
 def push_start_data(stu_id):
     global id_time
@@ -54,13 +54,15 @@ def push_end_data(stu_id):
     df12['시작시간']= pd.to_datetime(df12['시작시간'])
     timestamp11 = df12.iat[0,0]
     timestamp22 = df12.iat[0,1]
-    st.dataframe(df12)
+    
     bbb = datetime.now(timezone('Asia/Seoul'))
     bbb2 = bbb.timestamp()
     sungong = bbb2 - timestamp22
     st1 = datetime.fromtimestamp(timestamp22)
     et1 = datetime.fromtimestamp(bbb2)
-    
+    df12.iat[0,2] = bbb
+    df12.iat[0,3] = bbb2
+    st.dataframe(df12)
     id_time = f"{stu_id}_{et1.year}-{format(et1.month,'02')}-{format(et1.day,'02')} {format(et1.hour,'02')}:{format(et1.minute,'02')}:{format(et1.second,'02')}"
     doc_ref2 = db2.collection("sungong").document(id_time)
     doc_ref2.set({'학생id':stu_id,'시작시간':timestamp11,
